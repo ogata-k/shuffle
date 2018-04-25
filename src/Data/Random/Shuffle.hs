8b3327b274a2f24e55e589baa7589c1c9133fefa
ogata-k <ogtkzk@yahoo.co.jp>
2018-04-25 19:26:22 +0900

    make funcs
    why is doing 'autoShuffle []' warining

--- src/Data/Random/Shuffle.hs
+++ src/Data/Random/Shuffle.hs
@@ -1,5 +1,24 @@
-module Data.Random.Shuffle where
+module Data.Random.Shuffle(
+  shuffle
+, autoShuffle
+)where
 
 import System.Random
+import Data.List (nub, sortOn)
+import Control.Applicative ((<$>), (<*>))
 
+getNubRandomRs :: (Eq a, Enum a, RandomGen g) => g -> (a, a) -> [a]
+getNubRandomRs g (l, h) = map toEnum $ take (abs (hInt - lInt) + 1) $ nub $ randomRs (lInt, hInt) g
+    where
+        lInt = fromEnum l
+        hInt = fromEnum h
 
+shuffle :: (Eq a, Enum a, RandomGen g) => g -> [a] -> [a]
+shuffle _ [] = []
+shuffle g xs = map fst $ sortOn snd $ zip xs rs
+    where
+        rs = getNubRandomRs g (1, length xs)
+
+autoShuffle :: (Eq a, Enum a) => [a] -> IO [a]
+autoShuffle [] = pure []
+autoShuffle xs = shuffle <$> newStdGen <*> (pure xs)
